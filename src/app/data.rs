56b34eaf5685bf66cdab447b30ac95680d86a22b
flamboyantpenguin <mail@flamboyantpenguin.in>
2026-09-15 00:41:38 +0530

    fix(app): changed xdg to directories for cross-platform support

--- src/app/data.rs
+++ src/app/data.rs
@@ -1,4 +1,5 @@
-use std::fs::File;
+use directories::ProjectDirs;
+use std::fs::{self, File};
 
 use crate::{
     app::{blog, elog, wlog},
@@ -6,36 +7,46 @@ use crate::{
 };
 
 pub fn load_config() {
-    let xdg_dirs = xdg::BaseDirectories::with_prefix(app_static().app_name);
+    let Some(dirs) = ProjectDirs::from("", "", app_static().app_name) else {
+        elog!("Unable to determine config directory");
+        return;
+    };
 
-    match xdg_dirs.get_config_file("prev.json") {
-        Some(file_path) => {
-            let Ok(file) = File::open(file_path) else {
-                wlog!("Previous config not found, must be new run");
-                return;
-            };
-            if let Ok(imported_data) = serde_json::from_reader::<_, AppConfig>(file) {
-                CONFIG.set(imported_data).ok();
-                blog!("Previous config loaded");
-            } else {
-                elog!("Invalid config. Corruption perhaps?");
-            }
-        }
-        None => {}
+    let config_file = dirs.config_dir().join("prev.json");
+
+    let Ok(file) = File::open(config_file) else {
+        wlog!("Previous config not found, must be new run");
+        return;
+    };
+
+    if let Ok(imported_data) = serde_json::from_reader::<_, AppConfig>(file) {
+        CONFIG.set(imported_data).ok();
+        blog!("Previous config loaded");
+    } else {
+        elog!("Invalid config. Corruption perhaps?");
     }
 }
 
 pub fn save_config() {
-    let xdg_dirs = xdg::BaseDirectories::with_prefix(app_static().app_name);
-
-    let Ok(file_path) = xdg_dirs.place_config_file("prev.json") else {
-        elog!("Unable to create config directories");
+    let Some(dirs) = ProjectDirs::from("", "", app_static().app_name) else {
+        elog!("Unable to determine config directory");
         return;
     };
-    let Ok(file) = File::create(file_path) else {
+
+    let config_dir = dirs.config_dir();
+
+    if let Err(_) = fs::create_dir_all(config_dir) {
+        elog!("Unable to create config directories");
+        return;
+    }
+
+    let config_file = config_dir.join("prev.json");
+
+    let Ok(file) = File::create(config_file) else {
         elog!("Unable to open/create config");
         return;
     };
+
     if let Err(_) = serde_json::to_writer_pretty(file, app_config()) {
         elog!("Unable to write config");
         return;
