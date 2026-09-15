7ff426baef91fffd1bf0f50520ce7fb977c735b7
flamboyantpenguin <mail@flamboyantpenguin.in>
2026-09-15 10:39:49 +0530

    feat(app): app now starts in fullscreen
    ToDo: Must change this to fullscreen only on starting listen on puppet

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ mod models;
 mod network;
 
 use gui::App;
+use iced::window;
 
 use crate::{app::blog, gui::NERD_FONT};
 
@@ -19,6 +20,10 @@ pub fn main() -> iced::Result {
         .subscription(App::subscription)
         .title(App::title)
         .theme(App::theme)
+        .window(window::Settings {
+            fullscreen: true,
+            ..Default::default()
+        })
         .default_font(NERD_FONT)
         .run()
 }
